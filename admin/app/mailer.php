41b4968c4acf68ac0a035c2a8a8ce57109aa7be9
userwww1537 <nguyentanytricker@gmail.com>
2023-11-18 20:46:41 +0700

    Fix bug PHPMailer

--- admin/app/mailer.php
+++ admin/app/mailer.php
@@ -9,7 +9,7 @@ use PHPMailer\PHPMailer\Exception;
 class Mailer {
     public function sendMail($title, $content, $addressMail) {
 
-    //Create an instance; passing `true` enables exceptions
+    //Create an instance; passing `true` enables exce ptions
     $mail = new PHPMailer(true);
     try {
         //Server settings
