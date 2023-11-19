31392a08e7563bfe361b3019dfe38a17871720f5
userwww1537 <nguyentanytricker@gmail.com>
2023-11-19 07:56:54 +0700

    Done Login Save Cookie

--- public/controllers/cookie_account.php
+++ public/controllers/cookie_account.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_SESSION['delCookie']) && $_SESSION['delCookie'] == True) {
-    setcookie("accountsave", True, 0);
+    setcookie("accountsave", "True", 0);
     unset($_COOKIE['accountsave']);
     unset($_SESSION['83x86']);
     unset($_SESSION['delCookie']);
 }
 
-if (isset($_COOKIE['accountsave']) && $_COOKIE['accountsave'] != True && !isset($_SESSION['83x86'])) {
+if (isset($_COOKIE['accountsave']) && $_COOKIE['accountsave'] != "True" && !isset($_SESSION['83x86'])) {
     include_once "model_dao/account.php";
     $a = new account_lass();
     $_SESSION['83x86'] = $a->update_cookie_ses($_COOKIE['accountsave']);
