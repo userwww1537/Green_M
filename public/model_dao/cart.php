04f1ad3fb17178d0ec1f6120cdbad802e75914b7
hung <zkanhung@gmail.com>
2023-12-13 08:56:31 +0700

    function

--- public/model_dao/cart.php
+++ public/model_dao/cart.php
@@ -9,15 +9,6 @@
             return $this->conn_execute($sql, $a, $b, $c, $d, $e, $_SESSION['83x86']['account_id'], $f);
         }
 
-        public function show_cart($a) {
-            $sql = "SELECT cart.*, account.account_name as shop_name
-                    FROM cart
-                    JOIN account ON cart.shop_id = account.account_id
-                    WHERE cart.account_id = ?
-            ";
-            return $this->conn_show_all($sql, $a);
-        }
-
         public function add_cart($a, $b, $c, $d, $e, $f) {
             $sql = "INSERT INTO cart(cart_name, cart_price, cart_img, cart_qty, product_id, account_id, shop_id) VALUES(?, ?, ?, ?, ?, ?, ?)";
             return $this->conn_execute($sql, $a, $b, $c, $d, $e, $_SESSION['83x86']['account_id'], $f);
@@ -28,9 +19,6 @@
             return $this->conn_execute($sql, $a, $b);
         }
 
-        public function del_cart($a) {
-            $sql = "DELETE FROM cart WHERE cart_id = ?";
-            return $this->conn_execute($sql, $a);
         }
-    }
+    
 ?>
